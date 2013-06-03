62b9054deccc04cd1d5eb21801908ad8c9039a6e
Teemu Likonen <tlikonen@iki.fi>
2013-06-03 08:51:07 +0300

    Remove "aims to provide" part

--- make-doc.lisp
+++ make-doc.lisp
@@ -63,10 +63,9 @@ the following checkers:
   - Voikko (Finnish)
   - AppleSpell (Mac OSX)
 
-_CL Enchant_ aim's to provide all or most of the Enchant's features. It
-uses [The Common Foreign Function Interface][CFFI] (CFFI) for accessing
-the Enchant C library. CL Enchant should work on any Common Lisp
-implementation which supports CFFI.
+_CL Enchant_ uses [The Common Foreign Function Interface][CFFI] (CFFI)
+for accessing the Enchant C library. CL Enchant should work on any
+Common Lisp implementation which supports CFFI.
 
 [Enchant]: http://www.abisource.com/projects/enchant/
 [CFFI]:    http://common-lisp.net/project/cffi/
