7856c93c6012fed72b9a2fff76ce9fa51ac21ed4
Teemu Likonen <tlikonen@iki.fi>
2019-03-17 14:22:37 +0200

    Reorganize make-doc.lisp

--- make-doc.lisp
+++ make-doc.lisp
@@ -1,43 +1,6 @@
 #-sbcl
 (error "This is only for SBCL.")
 
-(require :sb-posix)
-(require :sb-introspect)
-
-(flet ((probe-load (path &optional (default (user-homedir-pathname)))
-         (let ((path (merge-pathnames path default)))
-           (when (probe-file path) (load path))))
-       (funcallstr (string &rest args)
-         (apply (read-from-string string) args)))
-  (or (probe-load #p"quicklisp/setup.lisp")
-      (probe-load #p".quicklisp/setup.lisp")
-      (let ((url "http://beta.quicklisp.org/quicklisp.lisp")
-            (init (nth-value 1 (funcallstr "sb-posix:mkstemp"
-                                           "/tmp/quicklisp-XXXXXX"))))
-        (unwind-protect
-             (progn
-               (sb-ext:run-program "wget" (list "-O" init "--" url)
-                                   :search t :output t)
-               (when (probe-load init)
-                 (funcallstr "quicklisp-quickstart:install")))
-          (delete-file init)))))
-
-(defun symbol-doc-type (symbol)
-  (let (docs)
-    (flet ((doc (symbol type key)
-             (push (list symbol key (documentation symbol type)) docs)))
-      (cond ((ignore-errors (macro-function symbol))
-             (doc symbol 'function :macro))
-            ((ignore-errors (symbol-function symbol))
-             (doc symbol 'function :function)))
-      (when (ignore-errors (symbol-value symbol))
-        (doc symbol 'variable :variable))
-      (cond ((subtypep symbol 'condition)
-             (doc symbol 'type :condition))
-            ((ignore-errors (find-class symbol))
-             (doc symbol 'type :class))))
-    docs))
-
 (defparameter *head*
   "~
 Enchant
@@ -142,6 +105,43 @@ freeing operations.
 
 ")
 
+(require :sb-posix)
+(require :sb-introspect)
+
+(flet ((probe-load (path &optional (default (user-homedir-pathname)))
+         (let ((path (merge-pathnames path default)))
+           (when (probe-file path) (load path))))
+       (funcallstr (string &rest args)
+         (apply (read-from-string string) args)))
+  (or (probe-load #p"quicklisp/setup.lisp")
+      (probe-load #p".quicklisp/setup.lisp")
+      (let ((url "http://beta.quicklisp.org/quicklisp.lisp")
+            (init (nth-value 1 (funcallstr "sb-posix:mkstemp"
+                                           "/tmp/quicklisp-XXXXXX"))))
+        (unwind-protect
+             (progn
+               (sb-ext:run-program "wget" (list "-O" init "--" url)
+                                   :search t :output t)
+               (when (probe-load init)
+                 (funcallstr "quicklisp-quickstart:install")))
+          (delete-file init)))))
+
+(defun symbol-doc-type (symbol)
+  (let (docs)
+    (flet ((doc (symbol type key)
+             (push (list symbol key (documentation symbol type)) docs)))
+      (cond ((ignore-errors (macro-function symbol))
+             (doc symbol 'function :macro))
+            ((ignore-errors (symbol-function symbol))
+             (doc symbol 'function :function)))
+      (when (ignore-errors (symbol-value symbol))
+        (doc symbol 'variable :variable))
+      (cond ((subtypep symbol 'condition)
+             (doc symbol 'type :condition))
+            ((ignore-errors (find-class symbol))
+             (doc symbol 'type :class))))
+    docs))
+
 (defun print-doc (package &key (stream *standard-output*) (prefix "### "))
   (format stream *head*)
   (loop :with *package* := (find-package package)
@@ -175,8 +175,12 @@ freeing operations.
 (pushnew (make-pathname :directory (pathname-directory *load-pathname*))
          asdf:*central-registry*)
 
-(with-output-to-string (*standard-output*)
-  (ql:quickload "enchant"))
+(asdf:initialize-source-registry
+ (list :source-registry
+       :ignore-inherited-configuration
+       (list :directory *default-pathname-defaults*)))
+
+(ql:quickload "enchant" :silent t)
 
 (handler-case (print-doc "ENCHANT")
   (error (c)
